9b46432b6963f79d534e05007b7daa1abdbdb7e6
Julian de Bruin <jybruin@avans.nl>
2022-03-01 18:30:18 +0000

    Added CMake and all needed directories

---
+++ Mainboard/CMakeFiles/MainBoard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MainBoard.dir/src/main.cpp.o"
+  "MainBoard"
+  "MainBoard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MainBoard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Mainboard/src/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf( "PuzzleController\n" );
+
+	return 0;
+}
