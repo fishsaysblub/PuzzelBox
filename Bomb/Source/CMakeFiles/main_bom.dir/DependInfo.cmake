45af98e1f5b88c6436f6275660d3ffdfa3899e87
Jef <jefbaars@gmail.com>
2022-04-21 10:25:29 +0200

    Base webUI

---
+++ Bomb/Source/CMakeFiles/main_bom.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Biem.c" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Biem.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "inc"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/AssembleConnection.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/AssembleConnection.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Bom.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Bom.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Fuses.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Fuses.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/JumperInterconnect.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/JumperInterconnect.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Peripherals/Display.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Peripherals/Display.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Peripherals/Gpio.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Peripherals/Gpio.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Peripherals/I2Cinterface.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Peripherals/I2Cinterface.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Peripherals/Keypad.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Peripherals/Keypad.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Peripherals/Sound.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Peripherals/Sound.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Peripherals/SpiInterface.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Peripherals/SpiInterface.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Peripherals/StLedDriver.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Peripherals/StLedDriver.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/Peripherals/Tca8418Interface.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/Peripherals/Tca8418Interface.cpp.o"
+  "/home/jef/Git/PuzzelBox/Bomb/Source/src/main.cpp" "/home/jef/Git/PuzzelBox/Bomb/Source/CMakeFiles/main_bom.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Bomb/Source/CMakeFiles/main_bom.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_bom.dir/src/AssembleConnection.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Biem.c.o"
+  "CMakeFiles/main_bom.dir/src/Bom.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Fuses.cpp.o"
+  "CMakeFiles/main_bom.dir/src/JumperInterconnect.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Peripherals/Display.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Peripherals/Gpio.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Peripherals/I2Cinterface.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Peripherals/Keypad.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Peripherals/Sound.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Peripherals/SpiInterface.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Peripherals/StLedDriver.cpp.o"
+  "CMakeFiles/main_bom.dir/src/Peripherals/Tca8418Interface.cpp.o"
+  "CMakeFiles/main_bom.dir/src/main.cpp.o"
+  "bin/main_bom"
+  "bin/main_bom.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main_bom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
