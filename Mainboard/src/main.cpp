46c2c88595d23278be76c7f3298db6a9384afb04
Julian de Bruin <jybruin@avans.nl>
2022-06-03 13:24:05 +0100

    Comment might be needed for later

--- Mainboard/src/main.cpp
+++ Mainboard/src/main.cpp
@@ -12,3 +12,61 @@ int main()
 
 	return 0;
 }
+
+
+
+/* Testing */
+
+	// #include "wiringPi.h"
+	// #include <softPwm.h>
+	// #include <stdio.h>
+
+	// #define G 27//16
+	// #define R 28//20
+	// #define B 29//21
+
+	/*wiringPiSetup();
+
+	pinMode( R, OUTPUT );
+	pinMode( G, OUTPUT );
+	pinMode( B, OUTPUT );
+	
+	softPwmCreate( R, 10, 255 );
+	softPwmCreate( G, 10, 255 );
+	softPwmCreate( B, 10, 255 );
+
+	while( 1 )
+	{
+		//156, 11, 144
+
+		for (int i = 0; i < 100; i++)
+		{
+			softPwmCreate( R, i, 100 );
+			delay( 25 );
+		}
+		
+		for (int i = 0; i < 100; i++)
+		{
+			softPwmCreate( G, i, 100 );
+			delay( 25 );
+		}
+
+		for (int i = 0; i < 100; i++)
+		{
+			softPwmCreate( B, i, 100 );
+			delay( 25 );
+		}
+
+		// digitalWrite( G, HIGH );
+		// delay(300);
+		// digitalWrite( R, LOW );
+		// delay(100);
+
+		// digitalWrite( G, HIGH );
+		// delay(300);
+		// digitalWrite( G, LOW );
+
+		// digitalWrite( B, HIGH );
+		// delay(300);
+		// digitalWrite( B, LOW );
+	}*/
